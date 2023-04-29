b9cc4bc7ccdf867929aaad63297fbc3bbec67e10
e.cockburn@students.ephec.be <116933854+Euan-lc@users.noreply.github.com>
2023-04-29 17:50:31 +0200

    auth0 provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.js';
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Auth0Provider domain={"dev-nlsmejlnkpumpmbb.eu.auth0.com"} clientId={"geYji3g7wIIjNHNS6J7QrQSJX5h5gUTJ"}>
     <React.StrictMode>
     <App />
     </React.StrictMode>
+    </Auth0Provider>
 );
