276ad8842d94125e05a50cc0015e20ce8824b0b1
e.cockburn@students.ephec.be <116933854+Euan-lc@users.noreply.github.com>
2023-05-09 20:58:05 +0200

    refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-import Home from './components/Home';
-// import Chevaux from './components/HorseList';
-import Calendrier from './components/Calendrier.js';
-import Abonnement from "./components/Abonnement";
 import Header from "./Header";
 import Footer from "./Footer";
-import Test from "./components/test";
 import './css/App.css'
+import Nav from "./Nav";
 
 //import ReactDOM from 'react-dom/client';
 
@@ -17,24 +12,7 @@ class App extends React.Component{
         return (
             <div id='App'>
                 <Header/>
-                <div id ='nav-bar'>
-                    <nav id='App-nav'>
-                    <ul id="ul-nav">
-                        <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
-                        <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
-                        <li className="nav"><Link to='/chevaux' className="chevaux">Chevaux</Link></li>
-                        <li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>
-                    </ul>
-                    </nav>
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route path="/calendrier" element={<Calendrier/>} />
-                        {/* <Route path="/chevaux" element={<Chevaux />} /> */}
-                        <Route path="/abonnement" element={<Abonnement />} />
-                        <Route path="/Test" element={<Test />} />
-
-                    </Routes>
-                </div>
+                <Nav/>
                 <Footer/>
             </div>
         ) ;
--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -2,70 +2,35 @@ import React from "react";
 import { Routes, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
-import Chevaux from './components/Chevaux';
+// import Chevaux from './components/HorseList';
 import Calendrier from './components/Calendrier.js';
 import Abonnement from "./components/Abonnement";
-
-import './css/Nav.css'
-
-import {AuthenticationGuard} from "./components/auth0/authentication-guard";
-import {useAuth0} from "@auth0/auth0-react";
-import {Error403} from "./components/auth0/error403";
-import AuthorizeUser from "./components/auth0/AuthorizeUser";
 import AddHorse from "./components/AddHorse";
 
-// class Nav extends React.Component{
-//     render(){
-//         return(
-//             <div id ='nav-bar'>
-//                 <nav id='App-nav'>
-//                 <ul id="ul-nav">
-//                     <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
-//                     <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
-//                     <li className="nav"><Link to='/chevaux' className="chevaux">Chevaux</Link></li>
-//                     <li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>
-//                 </ul>
-//                 </nav>
-//                 <Routes>
-//                     <Route exact path="/" element={<Home />} />
-//                     <Route path="/calendrier" element={<Calendrier/>} />
-//                     <Route path="/chevaux" element={<Chevaux />} />
-//                     <Route path="/abonnement" element={<AuthenticationGuard component={Abonnement}/>} />
-//                 </Routes>
-//             </div>
-//         )
-//     }
-// }
+import {AuthenticationGuard} from "./components/auth0/authentication-guard";
+import {Error403} from "./components/auth0/error403";
+import AuthorizeUser from "./components/auth0/AuthorizeUser";
+
 
 const Nav = () => {
-    const { user, isAuthenticated } = useAuth0();
-
     return(
-                <div id ='nav-bar'>
-                    <nav id='App-nav'>
-                    <ul id="ul-nav">
-                        <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
-                        <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
-                        <li className="nav"><Link to='/chevaux' className="chevaux">Chevaux</Link></li>
-                        <li className="nav"><Link to='/addhorse' className="chevaux">Chevaux</Link></li>
-                        {/*{ isAuthenticated &&*/}
-                        {/*user['http://localhost:3000/roles'].includes('equiadmin') &&*/}
-                        {/*(<li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>)}*/}
-                        <AuthorizeUser userRole={'equiadmin'}
-                                       component={<li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>}
-                                       />
-                    </ul>
-                    </nav>
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route path="/calendrier" element={<Calendrier/>} />
-                        <Route path="/chevaux" element={<AuthenticationGuard component={Chevaux}/>} />
-                        <Route path="/addhorse" element={<AddHorse />} />
-                        <Route path="/abonnement" element={<Abonnement/>}/>
-                        <Route path={"/notAuthorized"} element={<Error403/>}/>
-
-                    </Routes>
-                </div>
+            <div id ='nav-bar'>
+                <nav id='App-nav'>
+                <ul id="ul-nav">
+                    <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
+                    <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
+                    <li className="nav"><Link to='/addhorse' className="chevaux">Chevaux</Link></li>
+                    <AuthorizeUser userRole={'equiadmin'} component={<li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>}/>
+                </ul>
+                </nav>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/calendrier" element={<Calendrier/>} />
+                    <Route path="/addhorse" element={<AddHorse />} />
+                    <Route path="/abonnement" element={<Abonnement/>}/>
+                    <Route path={"/notAuthorized"} element={<Error403/>}/>
+                </Routes>
+            </div>
     )
 }
 
