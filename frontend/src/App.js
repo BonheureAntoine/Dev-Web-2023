c409479c2a0977491a7b24487808ee62839e8fcb
e.cockburn@students.ephec.be <116933854+Euan-lc@users.noreply.github.com>
2023-05-09 20:41:05 +0200

    fix double router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,28 +16,26 @@ class App extends React.Component{
     render(){
         return (
             <div id='App'>
-                <BrowserRouter>
-                    <Header/>
-                    <div id ='nav-bar'>
-                        <nav id='App-nav'>
-                        <ul id="ul-nav">
-                            <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
-                            <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
-                            <li className="nav"><Link to='/chevaux' className="chevaux">Chevaux</Link></li>
-                            <li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>
-                        </ul>
-                        </nav>
-                        <Routes>
-                            <Route exact path="/" element={<Home />} />
-                            <Route path="/calendrier" element={<Calendrier/>} />
-                            {/* <Route path="/chevaux" element={<Chevaux />} /> */}
-                            <Route path="/abonnement" element={<Abonnement />} />
-                            <Route path="/Test" element={<Test />} />
-                            
-                        </Routes>
-                    </div>
-                    <Footer/>
-                </BrowserRouter>
+                <Header/>
+                <div id ='nav-bar'>
+                    <nav id='App-nav'>
+                    <ul id="ul-nav">
+                        <li className="nav"><Link to='/' className="acceuil">Acceuil</Link></li>
+                        <li className="nav"><Link to='/calendrier' className="calendrier">Calendrier</Link></li>
+                        <li className="nav"><Link to='/chevaux' className="chevaux">Chevaux</Link></li>
+                        <li className="nav"><Link to='/abonnement' className="abonnement">Abonnement</Link></li>
+                    </ul>
+                    </nav>
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route path="/calendrier" element={<Calendrier/>} />
+                        {/* <Route path="/chevaux" element={<Chevaux />} /> */}
+                        <Route path="/abonnement" element={<Abonnement />} />
+                        <Route path="/Test" element={<Test />} />
+
+                    </Routes>
+                </div>
+                <Footer/>
             </div>
         ) ;
     }
