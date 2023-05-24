e7af73a0a6abcb158254231bc972a2f8a5a3422e
Bonheure Antoine <he202146@students.ephec.be>
2023-05-24 22:01:32 +0200

    test route rename

--- backend/tests/calendrier.test.js
+++ backend/tests/calendrier.test.js
@@ -6,7 +6,7 @@ const app = require('../app');
 // Je fais des test sur la reponse de ce que je recois de ma demande get
 describe('Test the evenements API', () => {
     test('It should respond with 200 status code', async () => {
-        const response = await request(app).get('/api/evenements');
+        const response = await request(app).get('/api/evenement');
         //console.log(response)
         expect(response.statusCode).toBe(200);
         expect(response.status).toBe(200);
@@ -14,7 +14,7 @@ describe('Test the evenements API', () => {
         expect(response.req.method).toBe('GET');
     });
     test('It should use GET method', async () => {
-        const response = await request(app).post('/api/evenements');
+        const response = await request(app).post('/api/evenement');
         expect(response.statusCode).toBe(404); // Method Not Allowed
     });
 });
