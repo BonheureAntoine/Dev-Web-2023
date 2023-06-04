110a4d7f96510dc87f724a69b111bec5a227befe
Alassane Wone <a.wone@students.ephec.be>
2023-06-04 23:54:27 +0200

    essaie d'ajout en s3

---
+++ backend/routes/r_horse.js
@@ -0,0 +1,19 @@
+import { generateUploadURL } from '../s3.js'
+
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/cl_horse');
+const pool = require("../db");
+
+router.get('/', userCtrl.getHorses);
+router.get('/options', userCtrl.options);
+router.post('/', userCtrl.addHorse);
+
+router.get('/s3Url', async (req, res) => {
+    const url = await generateUploadURL()
+    res.send({url})
+})
+
+
+module.exports = router;
